830008d3acc2b608ceb38a2e2cb1b4dfb0d0f190
Theo-Hafsaoui <theo-hafsaoui@etud.univ-tln.fr>
2022-08-29 15:16:25 +0200

    install.sh: add base pkg

---
+++ laptop/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo pacman -S xorg nitrogen i3-gaps rofi i3status i3lock ttf-dejavu alacritty fish base-devel firefox feh pcman
+
+#Install of yay to have acces to AUR
+git clone git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+#install dotfile
+mkdir ~/.config
+mv alacritty ~/.config
+mv fish ~/.config
+mv i3 ~/.config
+mv nvim ~/.config
+mv pcman ~/.config
+mv polybar ~/.config
+mv rofi ~/.config
+
+#herald icon
+sudo mv heraldstorm.ttf /usr/share/fonts/
+yay nerd-fonts-complete
+fc-cache -vf
