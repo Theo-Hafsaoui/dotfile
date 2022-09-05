a2a6a47e2b2cd511a216c24587528f80316687e8
Theo-Hafsaoui <theo-hafsaoui@etud.univ-tln.fr>
2022-09-05 16:49:04 +0200

    flag to mkdir to avoir error

--- laptop/install.sh
+++ laptop/install.sh
@@ -3,14 +3,14 @@
 sudo pacman -S xorg nitrogen i3-gaps rofi i3status i3lock ttf-dejavu alacritty fish base-devel firefox feh pcman man-db man-pages texinfo&&
 
 #Install of yay to have acces to AUR
-git clone git clone https://aur.archlinux.org/yay.git
+git clone git clone https://aur.archlinux.org/yay.git&&
 cd yay
 makepkg -si
 cd ..
 rm -rf yay
 
 #install dotfile
-mkdir ~/.config
+mkdir -p ~/.config
 mv alacritty ~/.config
 mv fish ~/.config
 mv i3 ~/.config
