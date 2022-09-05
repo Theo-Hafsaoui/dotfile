a2bebad7374e64f2d030d1f922632a1b7fcd0fa0
Theo-Hafsaoui <theo-hafsaoui@etud.univ-tln.fr>
2022-09-05 17:03:38 +0200

    fix typo pcmanfm

--- laptop/install.sh
+++ laptop/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo pacman -S xorg nitrogen i3-gaps rofi i3status i3lock ttf-dejavu alacritty fish base-devel firefox feh pcman man-db man-pages texinfo&&
+sudo pacman -S xorg nitrogen i3-gaps rofi i3status i3lock ttf-dejavu alacritty fish base-devel firefox feh pcmanfm man-db man-pages texinfo&&
 
 #Install of yay to have acces to AUR
 git clone git clone https://aur.archlinux.org/yay.git&&
-cd yay
-makepkg -si
-cd ..
-rm -rf yay
+cd yay&&
+makepkg -si&&
+cd ..&&
+rm -rf yay&&
 
 #install dotfile
 mkdir -p ~/.config
