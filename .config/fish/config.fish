68dbadc70fcca7b47ea87d2f4cdd4902c1d9337d
Theo-Hafsaoui <theo.hafsaoui@proton.me>
2024-10-01 17:20:19 +0200

    Fish: update conf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-#Mode VI
+
+
 alias p3="python3"
 fish_vi_key_bindings
-#OMF Theme
-set -g theme_color_scheme gruvbox
+set -g theme_color_scheme nord
 set -g theme_display_date no
+
+alias n="nvim"
